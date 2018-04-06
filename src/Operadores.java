45163503959c31b4a390acc069c776370e6e1aee
Joaquin Umpierrez <jo.umpierrez@gmail.com>
2018-04-05 22:02:42 -0300

    Terminado hasta Ejercicio 3

--- src/Operadores.java
+++ src/Operadores.java
@@ -9,9 +9,9 @@ public class Operadores
 	{
 		// Ejercicio 1
 		System.out.println("Ingrese un numero");
-		int numeroPrimo = entrada.nextInt();
+		int numeroA = entrada.nextInt();
 		System.out.print("\n");
-		if(EsPrimo(numeroPrimo))
+		if(EsPrimo(numeroA))
 		{
 			System.out.println("Es un numero Primo");
 		}
@@ -19,8 +19,22 @@ public class Operadores
 		{
 			System.out.println("No es un numero primo");
 		}
+		System.out.print("\n");
 		
 		// Ejercicio 2
+		System.out.println("Ingrese un numero");
+		int numeroB = entrada.nextInt();
+		System.out.print("\n");
+		System.out.println("El proximo numero primo es: " + PrimoMasCercano(numeroB));
+		System.out.print("\n");
+		
+		// Ejercicio 3
+		System.out.println("Ingrese un numero");
+		int numeroC = entrada.nextInt();
+		System.out.println("Ingrese un numero");
+		int numeroD = entrada.nextInt();
+		System.out.print("\n");
+		RestoYCociente(numeroC, numeroD);
 	}
 	
 	// Ejercicio 1
@@ -30,21 +44,84 @@ public class Operadores
 		int contador = 0;
 		int i;
 		
-		for(i = 1; i <= numero; i++)
-        {
-            if((numero % i) == 0)
-            {
-                contador++;
-            }
-        }
+		if (!(numero == 1))
+		{
+			for(i = 1; i <= numero; i++)
+	        {
+	            if((numero % i) == 0)
+	            {
+	                contador++;
+	            }
+	        }
+			
+			if(contador <= 2)
+	        {
+	            esValido = true;
+	        }
+		}
 		
-		if(contador <= 2)
-        {
-            esValido = true;
-        }
 		
 		return esValido;
 	}
 
+	// Ejejrcicio 2
+	public static int PrimoMasCercano(int numero)
+	{
+		int primo = 0;
+		int ultValor = numero+1;
+		
+		do
+		{
+			if (EsPrimo(ultValor))
+			{
+				primo = ultValor;
+			}
+			else
+			{
+				ultValor++;
+			}
+					
+		}
+		while (!EsPrimo(ultValor));
+		
+		return primo;
+	}
+	
+	// Ejercicio 3
+	public static void RestoYCociente(int numA, int numB)
+	{
+		int cociente = 0;
+		int resto = numA;
+		boolean positivo = true;
+		
+		if (numA < numB)
+		{
+			System.out.println("Resto - 0");
+			System.out.println("Cociente - 0");
+			System.out.print("\n");
+			
+		}
+		else
+		{
+			do
+			{
+				if((resto - numB >= 0))
+				{
+					cociente++;
+					resto = resto - numB;
+				}
+				else
+				{
+					positivo = false;
+				}
+			}
+			while(positivo == true);
+			
+			System.out.println("Resto - " + resto);
+			System.out.println("Cociente - " + cociente);
+			System.out.print("\n");
+		}
+	}
 
+	// Ejejrcicio 
 }
